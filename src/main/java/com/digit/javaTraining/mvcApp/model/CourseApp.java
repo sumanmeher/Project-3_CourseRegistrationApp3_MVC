4f99a909e5b8b5a180d023cf97f36cf6764fe9ee
Reeshu.Raj <Reeshu.Raj@LDIGITBGL-L0135.godigit.corp>
2023-08-03 18:42:02 +0530

    model and controller path

---
+++ src/main/java/com/digit/javaTraining/mvcApp/model/CourseApp.java
@@ -0,0 +1,60 @@
+package com.digit.javaTraining.mvcApp.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class CourseApp {
+	
+	public static Connection con;
+	
+	String cid;
+	String name;
+	int price;
+	String duration;
+	String description;
+	public String getCid() {
+		return cid;
+	}
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	public CourseApp() {
+		String url = "jdbc:mysql://localhost:3306/bankingApp";
+		String user = "root";
+		String pwd = "Dhoni$1234";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(url, user, pwd);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
