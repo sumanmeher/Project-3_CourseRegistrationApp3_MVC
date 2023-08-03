126cef4f78d2f5b66522652d576d6728af31d929
Reeshu.Raj <Reeshu.Raj@LDIGITBGL-L0135.godigit.corp>
2023-08-03 19:41:06 +0530

    professor Login

--- src/main/java/com/digit/javaTraining/mvcApp/controller/ProfessorLogin.java
+++ src/main/java/com/digit/javaTraining/mvcApp/controller/ProfessorLogin.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.digit.javaTraining.mvcApp.model.ProfessorModel;
 
@@ -14,11 +15,20 @@ import com.digit.javaTraining.mvcApp.model.ProfessorModel;
 public class ProfessorLogin extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		HttpSession session = req.getSession();
 		String username= req.getParameter("username");
 	    String password= req.getParameter("password");
 	    ProfessorModel professor = new ProfessorModel();
 	    professor.setUsername(username);
 	    professor.setPassword(password);
-	    professor.login(); 
+	    boolean isLogin = professor.login(); 
+	    if(isLogin) {
+	    	session.setAttribute("professor_username", professor.getUsername());
+	    	res.sendRedirect("professorMenu.jsp");
+	    }else {
+	    	session.setAttribute("failed_name", "Professor Login Failed");
+			session.setAttribute("failed_message", "Username and Password does not match");
+			res.sendRedirect("failed.jsp");
+	    }
 	}
 }
